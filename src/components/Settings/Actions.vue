148f9f522a8f7419281487e4ae2a6e50f24c3a29
Iledant <christophe.saintillan@gmail.com>
2020-03-15 21:50:47 +0100

    Refactor using new budget tables query

--- src/components/Settings/Actions.vue
+++ src/components/Settings/Actions.vue
@@ -14,7 +14,7 @@
         <v-flex xs12>
           <v-data-table
             :headers="headers"
-            :items="actionItems"
+            :items="actions"
             class="elevation-1"
             :loading="loading"
             dense
@@ -48,11 +48,11 @@
                     <v-icon>delete</v-icon>
                   </v-btn>
                 </td>
-                <td class="text-left">{{ item.chapter }}</td>
-                <td class="text-left">{{ item.sector }}</td>
-                <td class="text-center">{{ item.program }}</td>
-                <td class="text-center">{{ item.code }}</td>
-                <td class="text-left">{{ item.name }}</td>
+                <td>{{ item.chapter }}</td>
+                <td>{{ item.sector }}</td>
+                <td>{{ item.program }}</td>
+                <td class="text-right">{{ item.code }}</td>
+                <td>{{ item.name }}</td>
               </tr>
             </template>
           </v-data-table>
@@ -94,7 +94,7 @@ export default {
       { text: 'Chapitre', value: 'chapter' },
       { text: 'Secteur', value: 'sector' },
       { text: 'Programme', value: 'program' },
-      { text: 'Code action', value: 'code' },
+      { text: 'Code action', value: 'code', align: 'right' },
       { text: 'Intitulé', value: 'name' }
     ],
     search: '',
@@ -110,40 +110,7 @@ export default {
       sectors: state => state.budgetTables.sectorList,
       programs: state => state.budgetTables.programList,
       actions: state => state.budgetTables.actionList
-    }),
-    actionItems () {
-      const pList = this.programs
-      const cList = this.chapters
-      const sList = this.sectors
-      if (!pList ||
-        !pList.length ||
-        !cList ||
-        !cList.length ||
-        !sList ||
-        !sList.length
-      ) {
-        return []
-      }
-      return this.actions.map(a => {
-        const program = pList.find(p => p.id === a.program_id)
-        const chapter = cList.find(c => c.id === program.chapter_id)
-        const sector = sList.find(s => s.id === a.sector_id)
-        return {
-          id: a.id,
-          chapter_id: chapter.id,
-          chapter: chapter.code + ' - ' + chapter.name,
-          sector: sector.name,
-          program:
-            program.code_contract + program.code_function + program.code_number,
-          code:
-            program.code_contract +
-            program.code_function +
-            program.code_number +
-            a.code,
-          name: a.name
-        }
-      })
-    }
+    })
   },
   methods: {
     remove (item) {
