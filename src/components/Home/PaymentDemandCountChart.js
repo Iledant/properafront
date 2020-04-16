bea22228611850f924011411b725451888109b69
Iledant <christophe.saintillan@gmail.com>
2020-04-16 09:44:14 +0200

    Add green and red background to chart

--- src/components/Home/PaymentDemandCountChart.js
+++ src/components/Home/PaymentDemandCountChart.js
@@ -22,6 +22,22 @@ export default {
           cubicInterpolationMode: 'monotone',
           label: ' Évolution des traitements DC',
           data: []
+        },
+        {
+          backgroundColor: '#FFEBEE',
+          fill: true,
+          borderWidth: 0,
+          pointRadius: 0,
+          label: ' Augmentation du stock',
+          data: []
+        },
+        {
+          backgroundColor: '#E8F5E9',
+          fill: true,
+          borderWidth: 0,
+          pointRadius: 0,
+          label: ' Réduction du stock',
+          data: []
         }
       ]
     },
@@ -53,6 +69,14 @@ export default {
     paymentDemandCount (list) {
       this.datas.datasets[0].data = list.map(a => -a.uncontrolled)
       this.datas.datasets[1].data = list.map(a => -a.unprocessed)
+      let minScale = Math.min(0, ...this.datas.datasets[1].data, ...this.datas.datasets[0].data)
+      let maxScale = Math.max(0, ...this.datas.datasets[1].data, ...this.datas.datasets[0].data)
+      minScale -= 5 + (minScale) % 5
+      maxScale += 5 + maxScale % 5
+      this.options.scales.yAxes[0].ticks.min = minScale
+      this.options.scales.yAxes[0].ticks.max = maxScale
+      this.datas.datasets[2].data = (new Array(31)).fill(minScale)
+      this.datas.datasets[3].data = (new Array(31)).fill(maxScale)
       this.datas.labels = list.map(a =>
         new Date(a.date).toLocaleDateString('fr-FR', { day: '2-digit', month: '2-digit' }))
       this.renderChart(this.datas, this.options)
